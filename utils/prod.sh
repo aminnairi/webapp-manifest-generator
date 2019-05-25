60ff962c3e26acb630b44a6eed5e4044dc2615f4
Amin NAIRI <nairi.amin@gmail.com>
2019-05-25 13:08:56 +0200

    :pencil2: replaced all occurences of dist with docs

--- utils/prod.sh
+++ utils/prod.sh
@@ -3,14 +3,14 @@
 elm='./node_modules/.bin/elm'
 uglify='./node_modules/.bin/uglifyjs'
 input='./src/Main.elm'
-output='./dist/main.js'
+output='./docs/main.js'
 htmlmin='./node_modules/.bin/html-minifier'
 uglifycss='./node_modules/.bin/uglifycss'
 
-rm -rf ./dist
-mkdir ./dist
+rm -rf ./docs
+mkdir ./docs
 $elm make --optimize --output=$output $input
 $uglify $output --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | $uglify --mangle --output=$output
-$htmlmin --case-sensitive --collapse-boolean-attributes --collapse-inline-tag-whitespace --collapse-whitespace --conservative-collapse --decode-entities --quoteCharacter="'" --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace ./src/static/index.html > ./dist/index.html
-$uglify --output ./dist/index.js ./src/static/index.js
-$uglifycss ./src/static/index.css > ./dist/index.css
+$htmlmin --case-sensitive --collapse-boolean-attributes --collapse-inline-tag-whitespace --collapse-whitespace --conservative-collapse --decode-entities --quoteCharacter="'" --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace ./src/static/index.html > ./docs/index.html
+$uglify --output ./docs/index.js ./src/static/index.js
+$uglifycss ./src/static/index.css > ./docs/index.css
